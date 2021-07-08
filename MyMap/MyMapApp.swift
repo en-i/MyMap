a7183dfb27269492ec8ed58eb0f33c86541d47fc
en-i <templebond@icloud.com>
2021-07-09 01:27:40 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by terada enishi on 2021/07/09.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
